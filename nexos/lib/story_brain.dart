5e466d2a2e2b1cfaa68b628642f69857adc48362
Erica Ross <ericaross@Ericas-MacBook-Pro.local>
2021-05-23 14:46:35 -0400

    added new stories

--- nexos/lib/story_brain.dart
+++ nexos/lib/story_brain.dart
@@ -5,34 +5,34 @@ import 'story.dart';
 class StoryBrain {
 
 
-  List<Story> _storyData = [
+List<Story> _storyData = [
     Story(
         storyTitle:
-            'Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: "Need a ride, boy?".',
-        choice1: 'I\'ll hop in. Thanks for the help!',
-        choice2: 'Better ask him if he\'s a murderer first.'),
+            'La noche anterior, te sentías cansado/a así que te acostaste temprano para poder descansar suficiente y despertar temprano a terminar las tareas que tienes que entregar al día siguiente. Cuando te despiertas, te das cuenta que te levantaste tarde y no podrás terminar las tareas. Al ver tu teléfono, tu mejor amigo te escribió',
+        choice1: 'Decides no ir y te quedas en casa sin avisar',
+        choice2: 'Vas a clase y tratas de terminar las asignaciones'),
     Story(
-        storyTitle: 'He nods slowly, unphased by the question.',
-        choice1: 'At least he\'s honest. I\'ll climb in.',
-        choice2: 'Wait, I know how to change a tire.'),
-    Story(
-        storyTitle:
-            'As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box.',
-        choice1: 'I love Elton John! Hand him the cassette tape.',
-        choice2: 'It\'s him or me! You take the knife and stab him.'),
+        storyTitle: 'Cuando llegas a la escuela, te llaman del coro, hay un evento en la tarde, se les da un permiso para practicar todo el día. La primera voz no fue y tú eres su sustituto, por lo que, no entras al aula sino que vas directo al salón de actos. A las 11:30AM, cuando te toca entregar la tarea, el profesor te pide la tarea con carácter de urgencia pues el maestro  ha dicho que esa tarea tenía un valor del 50% del curso. ¿Qué haces? ',
+        choice1: 'Sales del curso, te escapas del colegio.',
+        choice2: 'Le dices que dejaste la tarea en casa'),
     Story(
         storyTitle:
-            'What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?',
+            'Sientes que no tienes muchos deseos de hacer la tarea en estos momentos. Como aún es temprano, decides ir a un centro de juegosy ver la película de Aquaman, que se estrenó hace un tiempo pero no habías tenido tiempo. Antes de terminar, ves unas llamadas perdidas de tu madre y tratas de comunicarte con ella. Cuando te responde, la escuchas agitada y te pregunta que donde estas.',
+        choice1: 'Le dices que estás en la casa terminando una tarea',
+        choice2: 'Le dices que estas en la escuela y debes colgar.'),
+    Story(
+        storyTitle:
+            'El Profesor no te cree piensa que estas mientiendo, pero como eres un buen estudiante te dice que lo entregues mañana.',
         choice1: 'Restart',
         choice2: ''),
     Story(
         storyTitle:
-            'As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in.',
+            'Cuando tu madre llega a casa te dice que la escuela la llamo, no volvera a confiar en ti jamas y que de ahora en adelante ella te llevara a la escuela y no habra mas privacidad.',
         choice1: 'Restart',
         choice2: ''),
     Story(
         storyTitle:
-            'You bond with the murderer while crooning verses of "Can you feel the love tonight". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: "Try the pier".',
+            'Tu madre te dice que la llamaron de la escuela y estaba preocupada. Sin embargo, te dice que, estarás castigado por el resto de la semana y que, irá contigo en la tarde a la escuela para disculparte con tus maestros y decir la verdad.',
         choice1: 'Restart',
         choice2: '')
   ];
