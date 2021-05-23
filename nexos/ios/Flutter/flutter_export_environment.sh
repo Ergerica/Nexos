5a45227b563cb1a2cd0757244d35e4e017835a4e
Erica Ross <ericaross@Ericas-MacBook-Pro.local>
2021-05-22 22:40:09 -0400

    removed todo

--- nexos/ios/Flutter/flutter_export_environment.sh
+++ nexos/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/ericaross/Flutter SDK/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/ericaross/Downloads/destini-challenge-starting-master"
+export "FLUTTER_APPLICATION_PATH=/Users/ericaross/Documents/Nexos/nexos"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
--- nexos/lib/main.dart
+++ nexos/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:destini_challenge_starting/story_brain.dart';
 import 'package:flutter/material.dart';
 
-//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
+
 
 void main() => runApp(Destini());
 
@@ -10,11 +10,12 @@ class Destini extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData.dark(),
       home: StoryPage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-//TODO: Step 9 - Create a new storyBrain object from the StoryBrain class.
+
 StoryBrain storyBrain = StoryBrain();
 
 class StoryPage extends StatefulWidget {
@@ -42,7 +43,7 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 12,
                 child: Center(
                   child: Text(
-                    //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
+                  
                     storyBrain.getStory(),
                     style: TextStyle(
                       fontSize: 25.0,
@@ -55,7 +56,7 @@ class _StoryPageState extends State<StoryPage> {
                 child: TextButton(
                   onPressed: () {
                     //Choice 1 made by user.
-                    //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
+                   
                     setState(() {
                         storyBrain.nextStory(1);
                       });
@@ -66,7 +67,7 @@ class _StoryPageState extends State<StoryPage> {
                     onSurface: Colors.grey,
                   ),
                   child: Text(
-                    //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
+            
                     storyBrain.getChoice1(),
                     style: TextStyle(
                       fontSize: 20.0,
@@ -95,7 +96,7 @@ class _StoryPageState extends State<StoryPage> {
                     },
                    
                     child: Text(
-                      //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
+                     
                       storyBrain.getChoice2(),
                       style: TextStyle(
                         fontSize: 20.0,
@@ -112,6 +113,3 @@ class _StoryPageState extends State<StoryPage> {
   }
 }
 
-//TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
-
-//TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
